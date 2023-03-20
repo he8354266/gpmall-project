0b9a7ed19260d089dc3fa87147f24235beef1eb9
heyang <453159575@qq.com>
2023-03-20 09:47:09 +0800

    购物车完善

--- shopping-service/shopping-provider/src/main/java/com/gpmall/shopping/services/ProductServiceImpl.java
+++ shopping-service/shopping-provider/src/main/java/com/gpmall/shopping/services/ProductServiceImpl.java
@@ -1,10 +1,15 @@
 package com.gpmall.shopping.services;
 
+import com.alibaba.fastjson.JSON;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.gpmall.shopping.IProductService;
 import com.gpmall.shopping.constans.ShoppingRetCode;
 import com.gpmall.shopping.constant.GlobalConstants;
 import com.gpmall.shopping.converter.ContentConverter;
 import com.gpmall.shopping.converter.ProductConverter;
+import com.gpmall.shopping.dal.entitys.Item;
+import com.gpmall.shopping.dal.entitys.ItemDesc;
 import com.gpmall.shopping.dal.persistence.ItemDescMapper;
 import com.gpmall.shopping.dal.persistence.ItemMapper;
 import com.gpmall.shopping.dal.persistence.PanelContentMapper;
@@ -13,10 +18,14 @@ import com.gpmall.shopping.dto.*;
 import com.gpmall.shopping.services.cache.CacheManager;
 import com.gpmall.shopping.utils.ExceptionProcessorUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @Description //TODO
@@ -48,16 +57,63 @@ public class ProductServiceImpl implements IProductService {
         response.setCode(ShoppingRetCode.SUCCESS.getCode());
         response.setMsg(ShoppingRetCode.SUCCESS.getMessage());
         try {
-            cacheManager.checkCache(generatorProduceCacheKey(request));
+            String json = cacheManager.checkCache(generatorProduceCacheKey(request));
+            if (StringUtils.isNotBlank(json)) {
+                ProductDetailDto productDetailDto = JSON.parseObject(json, ProductDetailDto.class);
+                cacheManager.expire(generatorProduceCacheKey(request), GlobalConstants.PRODUCT_ITEM_EXPIRE_TIME);
+                response.setProductDetailDto(productDetailDto);
+                return response;
+            }
+            Item item = itemMapper.selectByPrimaryKey(request.getId().longValue());
+            ProductDetailDto productDetailDto = new ProductDetailDto();
+            productDetailDto.setProductId(request.getId().longValue());
+            productDetailDto.setProductName(item.getTitle());
+            productDetailDto.setSubTitle(item.getSellPoint());
+            productDetailDto.setLimitNum(ObjectUtils.isEmpty(item.getNum()) ? item.getNum().longValue() : item.getLimitNum().longValue());
+            productDetailDto.setSalePrice(item.getPrice());
+
+            ItemDesc itemDesc = itemDescMapper.selectByPrimaryKey(request.getId().longValue());
+            productDetailDto.setDetail(itemDesc.getItemDesc());
+            if (StringUtils.isNotBlank(item.getImage())) {
+                String images[] = item.getImage().split(",");
+                productDetailDto.setProductImageBig(images[0]);
+                productDetailDto.setProductImageSmall(Arrays.asList(images));
+            }
+            response.setProductDetailDto(productDetailDto);
+            //设置缓存
+            cacheManager.setCache(generatorProduceCacheKey(request), JSON.toJSON(productDetailDto).toString(), GlobalConstants.PRODUCT_ITEM_EXPIRE_TIME);
+
         } catch (Exception e) {
             log.error("ProductServiceImpl.getProductDetail Occur Exception :" + e);
             ExceptionProcessorUtils.wrapperHandlerException(response, e);
         }
-        return null;
+        return response;
     }
 
     @Override
     public AllProductResponse getAllProduct(AllProductRequest request) {
+        AllProductResponse response = new AllProductResponse();
+        response.setCode(ShoppingRetCode.SUCCESS.getCode());
+        response.setMsg(ShoppingRetCode.SUCCESS.getMessage());
+        try {
+            PageHelper.startPage(request.getPage(), request.getSize());
+            String orderCol = "created";
+            String orderDir = "desc";
+            if (request.getSort().equals("1")) {
+                orderCol = "price";
+                orderDir = "asc";
+            } else {
+                orderCol = "price";
+                orderDir = "desc";
+            }
+            List<Item> items = itemMapper.selectItemFront(request.getCid(), orderCol, orderDir, request.getPriceGt(), request.getPriceLte());
+            PageInfo<Item> pageInfo = new PageInfo<>(items);
+            List<ProductDto> productDtosList = productConverter.items2Dto(items);
+            response.setProductDtoList(productDtosList);
+            response.setTotal(pageInfo.getTotal());
+        } catch (Exception e) {
+
+        }
         return null;
     }
 
