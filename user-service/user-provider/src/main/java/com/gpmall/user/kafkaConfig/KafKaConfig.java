e6525d6b5dc75e45df8377a833b2364c7b8950c7
heyang <453159575@qq.com>
2023-03-21 10:46:39 +0800

    购物车完善

--- user-service/user-provider/src/main/java/com/gpmall/user/kafkaConfig/KafKaConfig.java
+++ user-service/user-provider/src/main/java/com/gpmall/user/kafkaConfig/KafKaConfig.java
@@ -1,9 +1,13 @@
 package com.gpmall.user.kafkaConfig;
 
+import com.gpmall.user.registerVerification.KafKaRegisterSuccProducerFactory;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 /**
  * @Description //TODO
@@ -14,6 +18,11 @@ import org.springframework.context.annotation.Configuration;
 public class KafKaConfig {
     @Autowired
     private KafkaProperties kafkaProperties;
-//    @Bean
-//    public KafKaRegisterSuccProducerFactory kafKaRegisterSuccProducerFactory;
+
+    @Bean
+    public KafKaRegisterSuccProducerFactory kafKaRegisterSuccProducerFactory() {
+        Map<String, Object> producerProperties = kafkaProperties.buildProducerProperties();
+        producerProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+        return null;
+    }
 }
