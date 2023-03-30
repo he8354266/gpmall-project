e4c7cbe2227da1f9532894039c4535021135a23c
heyang <453159575@qq.com>
2023-03-30 10:00:17 +0800

    .

--- user-service/user-provider/src/main/java/com/gpmall/user/services/UserLoginServiceImpl.java
+++ user-service/user-provider/src/main/java/com/gpmall/user/services/UserLoginServiceImpl.java
@@ -1,17 +1,24 @@
 package com.gpmall.user.services;
 
 import com.gpmall.user.IUserLoginService;
+import com.gpmall.user.constants.SysRetCodeConstants;
+import com.gpmall.user.dal.entitys.Member;
 import com.gpmall.user.dal.persistence.MemberMapper;
 import com.gpmall.user.dal.persistence.UserMapper;
 import com.gpmall.user.dto.CheckAuthRequest;
 import com.gpmall.user.dto.CheckAuthResponse;
 import com.gpmall.user.dto.UserLoginRequest;
 import com.gpmall.user.dto.UserLoginResponse;
+import com.gpmall.user.utils.ExceptionProcessorUtils;
+import com.gpmall.user.utils.JwtTokenUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.dubbo.config.annotation.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import tk.mybatis.mapper.entity.Example;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @Description //TODO
@@ -29,11 +36,42 @@ public class UserLoginServiceImpl implements IUserLoginService {
 
     @Override
     public UserLoginResponse login(UserLoginRequest request) {
-        return null;
+        log.info("Begin UserLoginServiceImpl.login: request:" + request);
+        UserLoginResponse response = new UserLoginResponse();
+        try {
+            request.requestCheck();
+            Example example = new Example(Member.class);
+            example.createCriteria().andEqualTo("state", 1).andEqualTo("username", request.getUserName());
+            List<Member> members = memberMapper.selectByExample(example);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("UserLoginServiceImpl.login Occur Exception :" + e);
+            ExceptionProcessorUtils.wrapperHandlerException(response, e);
+
+        }
+        return response;
     }
 
     @Override
     public CheckAuthResponse validToken(CheckAuthRequest request) {
-        return null;
+        log.info("Begin UserLoginServiceImpl.validToken: request:" + request);
+        CheckAuthResponse response = new CheckAuthResponse();
+        response.setCode(SysRetCodeConstants.SUCCESS.getCode());
+        response.setMsg(SysRetCodeConstants.SUCCESS.getMessage());
+        try {
+            request.requestCheck();
+            String decodeMsg = JwtTokenUtils.builder().token(request.getToken()).build().freeJwt();
+            if (StringUtils.isNotBlank(decodeMsg)) {
+                log.info("validate success");
+                response.setMsg(decodeMsg);
+                return response;
+            }
+            response.setCode(SysRetCodeConstants.TOKEN_VALID_FAILED.getCode());
+            response.setMsg(SysRetCodeConstants.TOKEN_VALID_FAILED.getMessage());
+        } catch (Exception e) {
+            log.error("UserLoginServiceImpl.validToken Occur Exception :" + e);
+            ExceptionProcessorUtils.wrapperHandlerException(response, e);
+        }
+        return response;
     }
 }
---
+++ user-service/user-provider/src/main/java/com/gpmall/user/services/UserRegisterServiceImpl.java
@@ -0,0 +1,17 @@
+package com.gpmall.user.services;
+
+import com.gpmall.user.IUserRegisterService;
+import com.gpmall.user.dto.UserRegisterRequest;
+import com.gpmall.user.dto.UserRegisterResponse;
+
+/**
+ * @Description //TODO
+ * @Date 2023/3/30 9:59
+ * @Author hy
+ **/
+public class UserRegisterServiceImpl implements IUserRegisterService {
+    @Override
+    public UserRegisterResponse register(UserRegisterRequest request) {
+        return null;
+    }
+}
