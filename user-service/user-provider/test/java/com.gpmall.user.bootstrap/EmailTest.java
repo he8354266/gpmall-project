70c72d64ff3c4444c0f1f62883d2402fe230cd40
heyang <453159575@qq.com>
2023-04-03 17:02:26 +0800

    .

--- user-service/user-provider/test/java/com.gpmall.user.bootstrap/EmailTest.java
+++ user-service/user-provider/test/java/com.gpmall.user.bootstrap/EmailTest.java
@@ -1,10 +1,19 @@
 package com.gpmall.user.bootstrap;
 
+import com.gpmall.commons.tool.email.DefaultEmailSender;
+import com.gpmall.commons.tool.email.MailData;
+import com.gpmall.commons.tool.email.emailConfig.EmailConfig;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.lang.annotation.RetentionPolicy;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
 
 /**
  * @Description //TODO
@@ -14,4 +23,27 @@ import java.lang.annotation.RetentionPolicy;
 @RunWith(SpringRunner.class)
 @SpringBootTest(properties = "classpath://application.yml")
 public class EmailTest {
+    @Autowired
+    private DefaultEmailSender defaultEmailSender;
+    @Autowired
+    private EmailConfig emailConfig;
+
+    @Test
+    public void sendMail() throws Exception {
+        for (int i = 0; i <= 3; i++) {
+            MailData mailData = new MailData();
+            mailData.setToAddresss(Arrays.asList("shuaike@chinasie.com"));
+            mailData.setContent("hello");
+            mailData.setSubject("文件地址");
+            Map<String, Object> viewObj = new HashMap<>();
+            viewObj.put("url", "http://www.xxxxx.ddcom/register");
+            viewObj.put("title", "激活邮件");
+            mailData.setDataMap(viewObj);
+            mailData.setFileName("activeRegisterInfoHtmlTemplate.html");
+            mailData.setAttachFileNames(new Vector<>(Arrays.asList("F:\\testmain.txt", "F:\\TEST.txt")));
+            defaultEmailSender.sendHtmlMailUseTemplate(mailData);
+            Thread.sleep(2000);
+            System.out.println("发送" + i + "邮件");
+        }
+    }
 }
