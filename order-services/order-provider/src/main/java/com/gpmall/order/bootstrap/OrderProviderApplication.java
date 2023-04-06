70ef337d7c00989ba6cfcdccfd28010bfb38301f
heyang <453159575@qq.com>
2023-04-06 09:55:45 +0800

    .

---
+++ order-services/order-provider/src/main/java/com/gpmall/order/bootstrap/OrderProviderApplication.java
@@ -0,0 +1,18 @@
+package com.gpmall.order.bootstrap;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @Description //TODO
+ * @Date 2023/4/6 9:42
+ * @Author hy
+ **/
+@ComponentScan(basePackages = {"com.gpmall.order", "com.gpmall.commons.mq"})
+@MapperScan(basePackages = "com.gpmall.order.dal")
+@SpringBootApplication
+public class OrderProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderProviderApplication.class, args);
+    }
+}
