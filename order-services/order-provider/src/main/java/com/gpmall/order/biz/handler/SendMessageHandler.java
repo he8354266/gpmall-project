a012986397280b85b7f631ca72e170b32ba33d5e
heyang <453159575@qq.com>
2023-04-13 10:48:58 +0800

    .

--- order-services/order-provider/src/main/java/com/gpmall/order/biz/handler/SendMessageHandler.java
+++ order-services/order-provider/src/main/java/com/gpmall/order/biz/handler/SendMessageHandler.java
@@ -1,7 +1,10 @@
 package com.gpmall.order.biz.handler;
 
+import com.gpmall.commons.mq.producer.RabbitMessageProducer;
+import com.gpmall.order.biz.context.CreateOrderContext;
 import com.gpmall.order.biz.context.TransHandlerContext;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,6 +15,9 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class SendMessageHandler extends AbstractTransHandler {
+    @Autowired
+    private RabbitMessageProducer rabbitMessageProducer;
+
     @Override
     public boolean isAsync() {
         return false;
@@ -19,6 +25,14 @@ public class SendMessageHandler extends AbstractTransHandler {
 
     @Override
     public boolean handler(TransHandlerContext context) {
-        return false;
+        CreateOrderContext createOrderContext = (CreateOrderContext) context;
+        //订单发送到rabbitmq
+        try {
+            rabbitMessageProducer.send(createOrderContext.getOrderId());
+        } catch (Exception e) {
+            log.error("发送订单id:{}到延迟队列失败" + createOrderContext.getOrderId());
+            return false;
+        }
+        return true;
     }
 }
